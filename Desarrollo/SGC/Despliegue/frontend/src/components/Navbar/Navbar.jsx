f109fd434bd14e7c8c3945d0967c8ec14a564607
ronald <ronald.munayco@unmsm.edu.pe>
2025-05-26 15:45:05 -0500

    Navbar interactivo

--- Desarrollo/SGC/Despliegue/frontend/src/components/Navbar/Navbar.jsx
+++ Desarrollo/SGC/Despliegue/frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("menu");
+
   return (
     <div className='navbar'>
-        <img src={assets.logo_juliaFish} alt='' />
+        <img src={assets.logo_juliaFish} alt='' className='logo'/>
         <ul className='navbar-menu'>
-            <li>home</li>
-            <li>menu</li>
-            <li>app mobil</li>
-            <li>contactanos</li>
+            
+            <li onClick={()=>setMenu("home")}
+            className={menu==="home"?"active":""}
+            >
+                inicio
+            </li>
+
+            <li onClick={()=>setMenu("menu")}
+            className={menu==="menu"?"active":""}
+            >
+                menu
+            </li>
+
+            <li onClick={()=>setMenu("mobile-app")}
+            className={menu==="mobile-app"?"active":""}
+            >
+                app mobil
+            </li>
+
+            <li onClick={()=>setMenu("contact-us")}
+            className={menu==="contact-us"?"active":""}
+            >
+                contactanos
+            </li>
+
         </ul>
+
         <div className='navbar-right'>
             <img src={assets.search_icon} alt=''/>
             <div className='navbar-search-icon'>
