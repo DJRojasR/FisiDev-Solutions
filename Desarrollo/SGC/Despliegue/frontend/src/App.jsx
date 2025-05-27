82f479cc70309b7aea6fb73bdc6c149349f07901
ronald <ronald.munayco@unmsm.edu.pe>
2025-05-27 00:02:36 -0500

    Pagina de inicio - Footer agregado

--- Desarrollo/SGC/Despliegue/frontend/src/App.jsx
+++ Desarrollo/SGC/Despliegue/frontend/src/App.jsx
@@ -3,21 +3,26 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 
 function App() {  
 
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-          {/* Creamos una ruta con el path / y el componente Home */}
-          <Route path="/" element={<Home />} />
-          {/* Creamos una ruta con el path /cart y el componente Cart */}
-          <Route path="/cart" element={<Cart/>} />
-          {/* Creamos una ruta con el path /order y el componente PlaceOrder */}
-          <Route path='/order' element={<PlaceOrder/>} />
-        </Routes>
+    <div>
+      <div className='app'>
+        <Navbar/>
+        <Routes>
+            {/* Creamos una ruta con el path / y el componente Home */}
+            <Route path="/" element={<Home />} />
+            {/* Creamos una ruta con el path /cart y el componente Cart */}
+            <Route path="/cart" element={<Cart/>} />
+            {/* Creamos una ruta con el path /order y el componente PlaceOrder */}
+            <Route path='/order' element={<PlaceOrder/>} />
+          </Routes>
+      </div>
+      <Footer/>
     </div>
+    
   )
 }
 
