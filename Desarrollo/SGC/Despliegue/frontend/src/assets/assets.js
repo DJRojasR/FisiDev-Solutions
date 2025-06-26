696438621647b5ba75854ac3bcf58238d2cee7c3
Darwin Rojas <josteinrojas590@gmail.com>
2025-06-26 11:54:16 -0500

    Añadiendo cambios

--- Desarrollo/SGC/Despliegue/frontend/src/assets/assets.js
+++ Desarrollo/SGC/Despliegue/frontend/src/assets/assets.js
@@ -13,6 +13,9 @@ import menu_5 from "./menu_5.png";
 import menu_6 from "./menu_6.png";
 import menu_7 from "./menu_7.png";
 import menu_8 from "./menu_8.png";
+import menu_9 from "./menu_9.png";
+import menu_10 from "./menu_10.png";
+import menu_11 from "./menu_11.png";
 
 import food_1 from "./food_1.png";
 import food_2 from "./food_2.png";
@@ -118,6 +121,18 @@ export const assets = {
     {
       menu_name: "Bebidas",
       menu_image: menu_8,
+    },
+    {
+      menu_name: "Estofados",
+      menu_image: menu_9,
+    },
+    {
+      menu_name: "Porciones",
+      menu_image: menu_10,
+    },
+    {
+      menu_name: "Postres",
+      menu_image: menu_11,
     },
   ];
 
