a3f76b37dbd63e04d580650b3d5cca92c63b8fad
Darwin Rojas <josteinrojas590@gmail.com>
2025-07-29 11:18:27 -0500

    Haciendo un commit

--- Desarrollo/SGC/Despliegue/frontend/src/pages/Cart/Cart.jsx
+++ Desarrollo/SGC/Despliegue/frontend/src/pages/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
 import { useNavigate } from 'react-router-dom';
-
+/*Este es un cometario xd*/
 const Cart = () => {
 
   const{cartItems,food_list,removeFromCart, getTotalCartAmount,url} = useContext(StoreContext);
